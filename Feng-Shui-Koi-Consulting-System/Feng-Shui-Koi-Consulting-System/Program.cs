dbbea9221c8615840efab5757fe104daa4e263ec
haman1608 <manhmk3944@ut.edu.vn>
2024-09-21 13:08:18 +0700

    Hello

---
+++ Feng-Shui-Koi-Consulting-System/Feng-Shui-Koi-Consulting-System/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feng_Shui_Koi_Consulting_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
