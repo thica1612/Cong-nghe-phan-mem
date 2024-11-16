7171829dbc7cebc991d83b07c99b026e9c4662b8
Khanh Ngoc <ngocnhk1624@ut.edu.vn>
2024-11-16 23:59:28 +0700

    updatelayoutTransactionHistory&Promotion

--- KoiFarm/KoiFarm.Repositories/obj/Debug/net8.0/KoiFarm.Repositories.AssemblyInfo.cs
+++ KoiFarm/KoiFarm.Repositories/obj/Debug/net8.0/KoiFarm.Repositories.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiFarm.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c0b6e9321335303e4bc1edd32bac11de9a155e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492536352bae81190fe585c826e674c744635846")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiFarm.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiFarm.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KoiFarm/KoiFarm.Services/obj/Debug/net8.0/KoiFarm.Services.AssemblyInfo.cs
+++ KoiFarm/KoiFarm.Services/obj/Debug/net8.0/KoiFarm.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiFarm.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c0b6e9321335303e4bc1edd32bac11de9a155e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492536352bae81190fe585c826e674c744635846")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiFarm.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiFarm.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
